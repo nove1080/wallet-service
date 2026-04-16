a21756d1d02fe2fb4664352b18b4acef5b4f2e4f
나제법 <skwpqjq@gmail.com>
2026-04-17 00:20:12 +0900

    chore(Kafka): 설정 추가

--- src/main/java/com/payment/wallet_service/WalletServiceApplication.java
+++ src/main/java/com/payment/wallet_service/WalletServiceApplication.java
@@ -2,7 +2,9 @@ package com.payment.wallet_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class WalletServiceApplication {
 
