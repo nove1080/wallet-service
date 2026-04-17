6679ee2ebb140bf113e6b0df96f73467a907bff9
나제법 <skwpqjq@gmail.com>
2026-04-17 14:25:57 +0900

    feat(Entity): JpaWalletEntity 구현

---
+++ src/main/java/com/payment/wallet_service/wallet/infrastructure/jpa/entity/JpaWalletEntity.java
@@ -0,0 +1,39 @@
+package com.payment.wallet_service.wallet.infrastructure.jpa.entity;
+
+import com.payment.wallet_service.common.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@ToString(callSuper = true)
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(
+    name = "wallet",
+    uniqueConstraints = {@UniqueConstraint(name = "uk_wallet_member_id", columnNames = "member_id")}
+)
+public class JpaWalletEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "wallet_id", updatable = false)
+    private Long id;
+
+    private Long memberId;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance;
+}
