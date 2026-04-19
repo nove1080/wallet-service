5b575a2a757f4d2ea2fd48a3264aeba208f6dc45
나제법 <skwpqjq@gmail.com>
2026-04-19 15:10:21 +0900

    feat(Repository): Wallet 저장 로직 구현

--- src/main/java/com/payment/wallet_service/wallet/infrastructure/jpa/repository/JpaWalletRepositoryAdapter.java
+++ src/main/java/com/payment/wallet_service/wallet/infrastructure/jpa/repository/JpaWalletRepositoryAdapter.java
@@ -2,7 +2,9 @@ package com.payment.wallet_service.wallet.infrastructure.jpa.repository;
 
 import com.payment.wallet_service.wallet.domain.Wallet;
 import com.payment.wallet_service.wallet.infrastructure.jpa.entity.JpaWalletEntity;
+import com.payment.wallet_service.wallet.infrastructure.jpa.entity.JpaWalletTransactionEntity;
 import com.payment.wallet_service.wallet.infrastructure.jpa.mapper.JpaWalletMapper;
+import com.payment.wallet_service.wallet.infrastructure.jpa.mapper.JpaWalletTransactionMapper;
 import com.payment.wallet_service.wallet.repository.WalletRepository;
 import java.util.List;
 import java.util.Set;
@@ -10,22 +12,41 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Repository
 public class JpaWalletRepositoryAdapter implements WalletRepository {
 
     private final JpaWalletRepository jpaWalletRepository;
+    private final JpaWalletTransactionRepository jpaWalletTransactionRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Set<Wallet> selectWallets(Set<Long> sellerIds) {
         return jpaWalletRepository.findByMemberIdIn(sellerIds)
             .stream()
             .map(JpaWalletMapper::mapToDomain)
             .collect(Collectors.toSet());
     }
+
+    @Override
+    @Transactional
+    public void saveAll(List<Wallet> wallets) {
+        jpaWalletRepository.saveAll(wallets.stream()
+            .map(JpaWalletMapper::mapToEntity)
+            .toList());
+        jpaWalletTransactionRepository.saveAll(wallets.stream()
+                .flatMap(wallet -> wallet.transactions().stream())
+                .map(JpaWalletTransactionMapper::mapToEntity)
+                .toList());
+    }
 }
 
 interface JpaWalletRepository extends JpaRepository<JpaWalletEntity, Long> {
     List<JpaWalletEntity> findByMemberIdIn(Set<Long> memberIds);
+}
+
+interface JpaWalletTransactionRepository extends JpaRepository<JpaWalletTransactionEntity, Long> {
+
 }
