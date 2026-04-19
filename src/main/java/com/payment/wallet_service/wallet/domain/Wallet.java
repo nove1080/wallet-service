de05c9415802413445626ebae16fbfa3b74dd677
나제법 <skwpqjq@gmail.com>
2026-04-19 15:11:31 +0900

    feat(Wallet): 정산 도메인 구현

--- src/main/java/com/payment/wallet_service/wallet/domain/Wallet.java
+++ src/main/java/com/payment/wallet_service/wallet/domain/Wallet.java
@@ -1,40 +1,39 @@
 package com.payment.wallet_service.wallet.domain;
 
-import com.payment.wallet_service.common.domain.BaseTimeEntity;
-import jakarta.persistence.AttributeOverride;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import java.math.BigDecimal;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import lombok.experimental.SuperBuilder;
+import java.util.List;
+import java.util.function.Predicate;
+import lombok.Builder;
 
-@ToString(callSuper = true)
-@Getter
-@SuperBuilder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Entity
-@Table(uniqueConstraints = {
-    @UniqueConstraint(name = "uk_wallet_member_id", columnNames = "member_id"),
-})
-public class Wallet extends BaseTimeEntity {
+@Builder(toBuilder = true)
+public record Wallet(
+    Long id,
+    Long memberId,
+    BigDecimal balance,
+    List<WalletTransaction> transactions
+) {
 
-    @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    @Column(name = "wallet_id", updatable = false)
-    private Long id;
+    public Wallet settle(String orderId, List<PaymentOrder> paymentOrders) {
+        return this.toBuilder()
+            .transactions(
+                paymentOrders.stream()
+                    .filter(isMyTransaction())
+                    .map(it -> WalletTransaction.builder()
+                        .walletId(this.id)
+                        .orderId(orderId)
+                        .amount(BigDecimal.valueOf(it.amount()))
+                        .transactionType(TransactionType.CREDIT)
+                        .build()
+                    ).toList())
+            .balance(addBalance(paymentOrders))
+            .build();
+    }
 
-    private Long memberId;
+    private BigDecimal addBalance(List<PaymentOrder> paymentOrders) {
+        return balance.add(BigDecimal.valueOf(paymentOrders.stream().mapToLong(PaymentOrder::amount).sum()));
+    }
 
-    @Column(nullable = false, precision = 19, scale = 2)
-    private BigDecimal balance;
+    private Predicate<PaymentOrder> isMyTransaction() {
+        return it -> memberId.equals(it.sellerId());
+    }
 }
