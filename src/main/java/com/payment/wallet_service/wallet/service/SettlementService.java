75cd4469a43c67b52d536c5781a23cae0e654aeb
나제법 <skwpqjq@gmail.com>
2026-04-19 15:12:49 +0900

    feat(Settlement): 정산 서비스 구현

---
+++ src/main/java/com/payment/wallet_service/wallet/service/SettlementService.java
@@ -0,0 +1,49 @@
+package com.payment.wallet_service.wallet.service;
+
+import com.payment.wallet_service.wallet.domain.PaymentConfirmMessage;
+import com.payment.wallet_service.wallet.domain.PaymentOrder;
+import com.payment.wallet_service.wallet.domain.Wallet;
+import com.payment.wallet_service.wallet.domain.WalletEventMessage;
+import com.payment.wallet_service.wallet.domain.WalletEventMessage.MessageType;
+import com.payment.wallet_service.wallet.repository.WalletRepository;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SettlementService {
+
+    private final WalletRepository walletRepository;
+
+    public WalletEventMessage settle(PaymentConfirmMessage confirmMessage) {
+        List<Wallet> settledWallets = doSettle(confirmMessage);
+        walletRepository.saveAll(settledWallets);
+
+        return createWalletEventMessage(confirmMessage, MessageType.SETTLEMENT_SUCCESS);
+    }
+
+    private List<Wallet> doSettle(PaymentConfirmMessage confirmMessage) {
+        Map<Long, List<PaymentOrder>> paymentOrderMap = confirmMessage.paymentOrders().stream()
+            .collect(Collectors.groupingBy(PaymentOrder::sellerId));
+
+        Set<Wallet> wallets = walletRepository.selectWallets(confirmMessage.getSellerIds());
+
+        return wallets.stream()
+            .map(it -> {
+                List<PaymentOrder> orders = paymentOrderMap.getOrDefault(it.memberId(), List.of());
+                return it.settle(confirmMessage.orderId(), orders);
+            }).toList();
+    }
+
+    private static WalletEventMessage createWalletEventMessage(PaymentConfirmMessage confirmMessage, MessageType messageType) {
+        return WalletEventMessage.builder()
+            .type(messageType)
+            .orderId(confirmMessage.orderId())
+            .build();
+    }
+
+}
