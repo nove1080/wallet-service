7578f588e94b5cdd2716acbf24956f3f551762e1
나제법 <skwpqjq@gmail.com>
2026-04-22 20:41:46 +0900

    chore(dummy): 구매자 지갑 더미 데이터 스크립트 추가

--- src/main/resources/db/seed/R__seed_wallet.sql
+++ src/main/resources/db/seed/R__seed_wallet.sql
@@ -1,6 +1,6 @@
-INSERT INTO wallet (wallet_id, member_id, balance, created_at, updated_at) VALUES
-    (1, 1, 0, NOW(), NOW()),
-    (2, 2, 0, NOW(), NOW()),
-    (3, 3, 0, NOW(), NOW()),
-    (4, 4, 0, NOW(), NOW()),
-    (5, 5, 0, NOW(), NOW());
+INSERT INTO wallet (member_id, balance, created_at, updated_at) VALUES
+    (1, 0, NOW(), NOW()),
+    (2, 0, NOW(), NOW()),
+    (3, 0, NOW(), NOW()),
+    (4, 0, NOW(), NOW()),
+    (5, 0, NOW(), NOW());
